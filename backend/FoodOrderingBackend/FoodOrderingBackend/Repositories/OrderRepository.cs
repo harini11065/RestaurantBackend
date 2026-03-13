ce66ed56d318b4df5f9c00b73144cb04f25eac8e
Harini <harini11065@gmail.com>
2026-03-13 11:17:40 +0530

    adding repository

---
+++ backend/FoodOrderingBackend/FoodOrderingBackend/Repositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using FoodOrderingBackend.Data;
+using FoodOrderingBackend.Models;
+
+public class OrderRepository
+{
+    private readonly AppDbContext _context;
+    public OrderRepository(AppDbContext context) => _context = context;
+
+    public async Task<Order> AddOrderAsync(Order order)
+    {
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+        return order;
+    }
+
+    public async Task<List<Order>> GetOrdersAsync()
+        => await _context.Orders.Include(o => o.Items).ToListAsync();
+}
